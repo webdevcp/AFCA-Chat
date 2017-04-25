ad6f7934badf82d296bdfcb4c3a5a7574cd5ced6
Cathal Killeen <cathalkilleen@gmail.com>
2017-04-24 21:48:20 -0600

    Added Conversation class

---
+++ app/Conversation.java
@@ -0,0 +1,11 @@
+public class Conversation {
+    public Boolean isGroup;
+    public ArrayList<Person> members;
+    public ArrayList<Message> messages;
+
+    private String encryptKey;
+
+
+
+    
+}
