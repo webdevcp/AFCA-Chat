cc7259577ddcc5480208a182e65624452cb1c876
Cathal Killeen <cathalkilleen@gmail.com>
2017-04-25 15:12:53 -0600

    Added person constructor

--- app/Person.java
+++ app/Person.java
@@ -2,4 +2,13 @@ public class Person {
     public String email;
     public String username;
     public String hashedPassword;
+
+    public Person(String name){
+        this.username = name;
+    }
+
+    public Person(){
+        this("Anon");
+    }
+
 }
